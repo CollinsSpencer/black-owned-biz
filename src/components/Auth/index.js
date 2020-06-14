7ca3078c60b73ac37b4344a9ecd540604b7ce2ed
Spencer Collins <CollinsSpencer@users.noreply.github.com>
2020-06-14 03:31:06 -0500

    Create Navigatation component

---
+++ src/components/Auth/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Link } from '@material-ui/core';
+import { auth, useAuthenticated } from '../../helpers/auth';
+
+export const AuthActions = () => {
+  const { authenticated } = useAuthenticated();
+
+  if (authenticated) {
+    return (
+      <Button onClick={() => auth.signOut()} underline='none'>
+        Sign-out
+      </Button>
+    );
+  } else {
+    return (
+      <Button color='inherit'>
+        <Link
+          component={RouterLink}
+          to={`/signin`}
+          color='inherit'
+          underline='none'
+        >
+          Sign in
+        </Link>
+      </Button>
+    );
+  }
+};
