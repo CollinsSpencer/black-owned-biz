d66672e430a4a0ede1fe664cf70daa033602fc41
Spencer Collins <CollinsSpencer@users.noreply.github.com>
2020-06-15 11:59:30 -0500

    Update constants

--- src/components/CategorySelector/index.js
+++ src/components/CategorySelector/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link as RouterLink, useRouteMatch } from 'react-router-dom';
 import { Box, Grid, Link, Typography } from '@material-ui/core';
-import { categories } from '../../helpers/consts';
+import { categories } from '../../helpers/constants';
 import { SquareButton } from '../SquareButton';
 
-export const CategorySelector = () => {
+export const CategorySelector = ({ availableCategories }) => {
   let { url } = useRouteMatch();
+
+  const categoriesList =
+    !!availableCategories && Array.isArray(availableCategories)
+      ? Object.values(categories).filter((c) =>
+          availableCategories.includes(c.key)
+        )
+      : Object.values(categories);
 
   return (
     <Grid container spacing={1}>
-      {Object.values(categories).map(({ Icon, key, name }) => (
+      {categoriesList.map(({ Icon, key, name }) => (
         <Grid item>
           <Link component={RouterLink} to={`${url}/${key}`}>
-            <SquareButton variant='contained'>
+            <SquareButton variant='contained' color='secondary'>
               <Box m={3} mb={1}>
                 <Icon style={{ fontSize: 50 }} />
               </Box>
