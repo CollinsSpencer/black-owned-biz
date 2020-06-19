a815c94a35ddeb22a8dfcf62e16a26640c8f9f2e
Jake Koperski <wijakoperski@gmail.com>
2020-06-19 16:32:08 -0500

    Migrated link to CategoryButton

--- src/components/CategoryButton/index.js
+++ src/components/CategoryButton/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Link as RouterLink, useRouteMatch } from 'react-router-dom';
+import { Link } from '@material-ui/core';
 import './style.css';
 
 // TODO: Figure out if Button can be incorporated here
 export const CategoryButton = (props) => {
-    const { IconComponent, name, imagePath } = props;
+    const { IconComponent, name, imagePath, code } = props;
+    const { url } = useRouteMatch();
 
     return (
-        <div className='category-button'>
-            <div className='category-button-image' style={{ backgroundImage: `url('${imagePath}')` }}>
-                <IconComponent style={{ fontSize: 96 }} />
+        <Link component={RouterLink} to={`${url}/${code}`}>
+            <div className='category-button'>
+                <div className='category-button-image' style={{ backgroundImage: `url('${imagePath}')` }}>
+                    <IconComponent style={{ fontSize: 96 }} />
+                </div>
+                <div className='category-button-label'>
+                    {name.toUpperCase()}
+                </div>
             </div>
-            <div className='category-button-label'>
-                {name.toUpperCase()}
-            </div>
-        </div>
+        </Link>
     );
 };
--- src/components/CategorySelector/index.js
+++ src/components/CategorySelector/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { Link as RouterLink, useRouteMatch } from 'react-router-dom';
-import { Grid, Link } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { categories } from '../../helpers/constants';
 // TODO: Figure out how to better import CategoryButton
 import { CategoryButton } from '../CategoryButton/index';
 
 export const CategorySelector = ({ availableCategories }) => {
-  const { url } = useRouteMatch();
   const categoriesList = availableCategories.sort().map(name => categories[name]);
 
   return (
     <Grid container spacing={1}>
-      {categoriesList.map(({ Icon, key, name, imagePath }) => (
+      {categoriesList.map(({ Icon, name, imagePath, key }) => (
         <Grid item key={key}>
-          <Link component={RouterLink} to={`${url}/${key}`}>
-            <CategoryButton IconComponent={Icon} name={name} imagePath={imagePath} />
-          </Link>
+          <CategoryButton IconComponent={Icon} name={name} imagePath={imagePath} code={key} />
         </Grid>
       ))}
     </Grid>
